387d3a90414961f16c4d44ba3cf08550292ac38d
ramin_yz <yazdani76ramin@gmail.com>
2022-08-05 14:03:14 +0430

    deleting unnecessary files

--- excersizes/practice2.py
+++
@@ -1,4 +0,0 @@
-test = int(input())
-for x in range(test, 0, -1):
-    print(*range(x,0,-1),sep="")
-
--- excersizes/practice5.py
+++
@@ -1,4 +0,0 @@
-username,password = input("username : "),input("password : ")
-if username =="admin" and password=="admin" : print("Welcome on Admin")
-elif username=="admin": print("Wrong")
-else:print("hello ",username)
